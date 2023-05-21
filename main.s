5a073171cf6d3951195f12ed0675114a5cb57e68
Iñaki García <yarimfuego@gmail.com>
2023-05-20 21:53:11 -0600

    Delay function sapareted of main

--- main.s
+++ main.s
@@ -5,48 +5,8 @@
 	.include "ivt.s"
 	.include "gpio_map.inc"
 	.include "rcc_map.inc"
-
-	.section .text
- 	.align  1
- 	.syntax unified
- 	.thumb
- 	.global __main
-delay:
-        # Prologue
-        push    {r7} @ backs r7 up
-        sub     sp, sp, #28 @ reserves a 32-byte function frame
-        add     r7, sp, #0 @ updates r7
-        str     r0, [r7] @ backs ms up
-        # Body function
-        mov     r0, #255 @ ticks = 255, adjust to achieve 1 ms delay
-        str     r0, [r7, #16]
-# for (i = 0; i < ms; i++)
-        mov     r0, #0 @ i = 0;
-        str     r0, [r7, #8]
-        b       F3
-# for (j = 0; j < tick; j++)
-F4:     mov     r0, #0 @ j = 0;
-        str     r0, [r7, #12]
-        b       F5
-F6:     ldr     r0, [r7, #12] @ j++;
-        add     r0, #1
-        str     r0, [r7, #12]
-F5:     ldr     r0, [r7, #12] @ j < ticks;
-        ldr     r1, [r7, #16]
-        cmp     r0, r1
-        blt     F6
-        ldr     r0, [r7, #8] @ i++;
-        add     r0, #1
-        str     r0, [r7, #8]
-F3:     ldr     r0, [r7, #8] @ i < ms
-        ldr     r1, [r7]
-        cmp     r0, r1
-        blt     F4
-        # Epilogue
-        adds    r7, r7, #28
-        mov	    sp, r7
-        pop	    {r7}
-        bx	    lr
+	
+	.extern delay
 
 # This subrutine reads if buttons are pressed
 read_button_input:
@@ -201,6 +161,11 @@ reset_count:
 		pop		{lr}
 		bx		lr
 
+	.section .text
+ 	.align  1
+ 	.syntax unified
+ 	.thumb
+ 	.global __main
 __main:
 		push 	{r7, lr}
 		sub 	sp, sp, #16
