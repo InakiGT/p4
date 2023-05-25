c3d6d06f944c9235e9fbc4675db56d5578ddcc0f
Iñaki García <yarimfuego@gmail.com>
2023-05-25 12:22:48 -0600

    ivt fixed

--- ivt.s
+++ ivt.s
@@ -41,7 +41,7 @@
 .word   Default_Handler + 1        @  3 RTC
 .word   Default_Handler + 1        @  4 FLASH
 .word   Default_Handler + 1        @  5 RCC
-.word   EXTI0_IRQHandler + 1        @  6 EXTI0
+.word   Default_Handler + 1        @  6 EXTI0
 .word   Default_Handler + 1        @  7 EXTI1
 .word   Default_Handler + 1        @  8 EXTI2
 .word   Default_Handler + 1        @  9 EXTI3
