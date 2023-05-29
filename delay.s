f6a3715cd44609754647a180ede29aff174de6b4
Iñaki García <yarimfuego@gmail.com>
2023-05-29 16:58:13 -0600

    Delay func renewed

--- delay.s
+++ delay.s
@@ -4,40 +4,10 @@
  	.thumb
  	.global delay
 delay:
-        # Prologue
-        push    {r7} @ backs r7 up
-        sub     sp, sp, #28 @ reserves a 32-byte function frame
-        add     r7, sp, #0 @ updates r7
-        str     r0, [r7] @ backs ms up
-        # Body function
-        mov     r0, #255 @ ticks = 255, adjust to achieve 1 ms delay
-        str     r0, [r7, #16]
-# for (i = 0; i < ms; i++)
-        mov     r0, #0 @ i = 0;
-        str     r0, [r7, #8]
-        b       F3
-# for (j = 0; j < tick; j++)
-F4:     mov     r0, #0 @ j = 0;
-        str     r0, [r7, #12]
-        b       F5
-F6:     ldr     r0, [r7, #12] @ j++;
-        add     r0, #1
-        str     r0, [r7, #12]
-F5:     ldr     r0, [r7, #12] @ j < ticks;
-        ldr     r1, [r7, #16]
-        cmp     r0, r1
-        blt     F6
-        ldr     r0, [r7, #8] @ i++;
-        add     r0, #1
-        str     r0, [r7, #8]
-F3:     ldr     r0, [r7, #8] @ i < ms
-        ldr     r1, [r7]
-        cmp     r0, r1
-        blt     F4
-        # Epilogue
-        adds    r7, r7, #28
-        mov	    sp, r7
-        pop	    {r7}
-        bx	    lr
+        mov r10, r0
+loop:
+        cmp r10, #0 
+        bne loop
+        bx lr
 
 .size   delay, .-delay
--- exti_isr.s
+++ exti_isr.s
@@ -1,5 +1,7 @@
 .include "exti_map.inc"
 
+.extern delay
+
 .cpu cortex-m3      @ Generates Cortex-M3 instructions
 .section .text
 .align	1
@@ -7,15 +9,22 @@
 .thumb
 .global EXTI0_Handler
 EXTI0_Handler:
+    push {lr}
+    mov     r0, #500
+    bl      delay
     adds    r8, r8, #1
     ldr     r0, =EXTI_BASE
     ldr     r1, [r0, EXTI_PR_OFFSET]
     orr     r1, r1, 0x40
     str     r1, [r0, EXTI_PR_OFFSET]
+    pop     {lr}
     bx      lr
 
 .global EXTI4_Handler
 EXTI4_Handler:
+    push    {lr}
+    mov     r0, #500
+    bl      delay
     cmp     r9, #1
     beq     .L0
     mov     r9, #1
@@ -27,4 +36,5 @@ EXTI4_Handler:
     ldr     r1, [r0, EXTI_PR_OFFSET]
     orr     r1, r1, 0x400
     str     r1, [r0, EXTI_PR_OFFSET]
+    pop     {lr}
     bx      lr
