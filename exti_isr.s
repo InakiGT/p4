0972d58ac589ca262004019623f3e81863308f63
Iñaki García <yarimfuego@gmail.com>
2023-05-29 17:00:34 -0600

    EXTI don't have frame

--- exti_isr.s
+++ exti_isr.s
@@ -9,22 +9,15 @@
 .thumb
 .global EXTI0_Handler
 EXTI0_Handler:
-    push {lr}
-    mov     r0, #500
-    bl      delay
     adds    r8, r8, #1
     ldr     r0, =EXTI_BASE
     ldr     r1, [r0, EXTI_PR_OFFSET]
     orr     r1, r1, 0x40
     str     r1, [r0, EXTI_PR_OFFSET]
-    pop     {lr}
     bx      lr
 
 .global EXTI4_Handler
 EXTI4_Handler:
-    push    {lr}
-    mov     r0, #500
-    bl      delay
     cmp     r9, #1
     beq     .L0
     mov     r9, #1
@@ -36,5 +29,4 @@ EXTI4_Handler:
     ldr     r1, [r0, EXTI_PR_OFFSET]
     orr     r1, r1, 0x400
     str     r1, [r0, EXTI_PR_OFFSET]
-    pop     {lr}
     bx      lr
