1e1d3447f7a666f52b17615c91a1ea64101fee99
Iñaki García <yarimfuego@gmail.com>
2023-05-29 15:58:42 -0600

    Testing EXTI4

--- exti_isr.s
+++ exti_isr.s
@@ -16,7 +16,8 @@ EXTI0_Handler:
 
 .global EXTI4_Handler
 EXTI4_Handler:
-    eor     r9, r9, #1
+   # eor     r9, r9, #1
+   mov      r9, #0
     ldr     r0, =EXTI_BASE
     ldr     r1, [r0, EXTI_PR_OFFSET]
     orr     r1, r1, 0x400
