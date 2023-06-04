36240345644a2b0d0fbe425c69931246e6c1ed28
Iñaki García <yarimfuego@gmail.com>
2023-06-04 17:47:07 -0600

    Values of EXTI_PR changed

--- exti_isr.s
+++ exti_isr.s
@@ -10,31 +10,19 @@
 .thumb
 .global EXTI0_Handler
 EXTI0_Handler:
-    ldr     r1, =GPIOA_BASE
-    ldr     r1, [r1, GPIOx_IDR_OFFSET]
-    and     r1, r1, 0x1
-    cmp     r1, 0x1
-    bne     .L0
     adds    r8, r8, #1
-.L0:
     ldr     r0, =EXTI_BASE
     ldr     r1, [r0, EXTI_PR_OFFSET]
-    orr     r1, r1, 0x40
+    orr     r1, r1, 0x1
     str     r1, [r0, EXTI_PR_OFFSET]
     bx      lr
 
 .global EXTI4_Handler
 EXTI4_Handler:
-    ldr     r1, =GPIOA_BASE
-    ldr     r1, [r1, GPIOx_IDR_OFFSET]
-    and     r1, r1, 0x10
-    cmp     r1, 0x10
-    bne     .L1
     eor     r5, r5, #1
     and     r5, r5, #1
-.L1:
     ldr     r0, =EXTI_BASE
     ldr     r1, [r0, EXTI_PR_OFFSET]
-    orr     r1, r1, 0x400
+    orr     r1, r1, 0x5
     str     r1, [r0, EXTI_PR_OFFSET]
     bx      lr
